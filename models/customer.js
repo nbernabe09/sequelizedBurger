830bf2e989721ac9515042469ae4e827bd327037
Nathan Bernabe <nbernabe09@aol.com>
2017-09-30 20:45:35 -0700

    Removed customer.js for now

--- models/customer.js
+++ models/customer.js
@@ -1,20 +1,20 @@
-// module.exports = function(sequelize, DataTypes) {
-//   var Customer = sequelize.define("Customer", {
-//     id: {
-//       type: DataTypes.INTEGER,
-//       primaryKey: true,
-//       autoIncrement: true,
-//       allowNull: false
-//     },
-//     customer_name: {
-//       type: DataTypes.STRING
-//     },
-//     burger_eaten: {
-//       type: DataTypes.STRING,
-//       allowNull: false
-//     }
-//   }, {
-//     timestamps: true
-//   });
-//   return Customer;
-// }
+module.exports = function(sequelize, DataTypes) {
+  var Customer = sequelize.define("Customer", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    customer_name: {
+      type: DataTypes.STRING
+    },
+    burger_eaten: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    timestamps: true
+  });
+  return Customer;
+}
